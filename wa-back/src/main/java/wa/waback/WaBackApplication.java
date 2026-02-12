0fa291b8aacfa9de658eeaef27ae8a670b5d8ec0
rtg <rtg2541@gmail.com>
2026-02-13 08:48:41 +0900

    [기타] 백엔드 테스트 프로젝트 생성

---
+++ wa-back/src/main/java/wa/waback/WaBackApplication.java
@@ -0,0 +1,13 @@
+package wa.waback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaBackApplication.class, args);
+    }
+
+}
---
+++ wa-back/src/test/java/wa/waback/WaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package wa.waback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
